750b0c39381fe1b4871ef817fa0b55ac7b8a6076
ClaudioSotelo <cmsp11000@hotmail.com>
2021-08-23 11:19:52 -0500

    Arreglado bug del git

---
+++ app/navigations/Navigation.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Icon } from "react-native-elements";
+import RestaurantsStack from "./RestaurantsStack";
+import FavoriteStack from "./FavoritesStack";
+import TopRestaurantsStack from "./TopRestaurantsStack";
+import SearchStack from "./SearchStack";
+import AccountStack from "./AccountStack";
+
+
+const Tab = createBottomTabNavigator();
+
+export default function Navigation()
+{
+    return (
+        <NavigationContainer>
+            <Tab.Navigator 
+            initialRouteName="restaurants"
+             screenOptions={({route}) => ({
+                 tabBarActiveTintColor:"#00a680",
+                 tabBarInactiveTintColor:"#646464",
+                 tabBarStyle:[
+                     {
+                        display: "flex"
+                     },
+                     null
+                 ],
+                 headerShown:false,
+                 tabBarIcon: ({color}) => screenOptions(route,color),
+             })}
+            >
+                <Tab.Screen name = "restaurants" component={RestaurantsStack} options={{title:"Restaurantes"}}/>
+                <Tab.Screen name = "favorites" component={FavoriteStack} options={{title:"Favoritos"}}/>
+                <Tab.Screen name = "top-restaurants" component={TopRestaurantsStack} options={{title:"Top 5"}}/>
+                <Tab.Screen name = "search" component={SearchStack} options={{title:"Buscar"}}/>
+                <Tab.Screen name = "account" component={AccountStack} options={{title:"Cuenta"}}/>
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+}
+
+// Se ha diferenciado el nombre de los iconos del tab con los de los Stacks//
+
+function screenOptions(route, color){
+    let iconName;
+    
+    switch(route.name){
+        case "restaurants":
+            iconName = "compass-outline"
+            break;
+        case "favorites":
+            iconName = "heart-outline"
+            break;
+        case "top-restaurants":
+            iconName = "star-outline"
+            break;
+        case "search":
+            iconName = "magnify"
+            break;
+        case "account":
+            iconName = "home-outline"
+            break;
+        default:
+            break;
+    }
+    return(
+        <Icon type="material-community" name={iconName} size={22} color={color} />
+    );
+}
